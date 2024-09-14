cfea6d59f4733101d45f535da2340cf4cf5a1b3b
Eryn Li <erynli@utexas.edu>
2024-09-14 12:32:50 -0500

    Add link to users page from home

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 // import BookCard from "./components/BookCard";
 import ProductCard from "./components/ProductCard/ProductCard";
 import styles from "./page.module.css";
-// import Link from "next/link";
+import Link from "next/link";
 
 export default function Home() {
   return (
       <main className={styles.main}>
         <h1>Next Read</h1>
-        {/* <Link href="/users">Users</Link> */}
         <p>Welcome to your personalized reading list!</p>
         {/* <BookCard /> */}
+        <Link href="/users">Users</Link>
         <ProductCard />
       </main>
   );

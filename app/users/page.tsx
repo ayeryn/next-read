068b93d0ae7023912d7cc12564f031bb9b059c5d
Eryn Li <erynli@utexas.edu>
2024-09-14 10:42:03 -0500

    dynamically render users page

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,17 +6,23 @@ interface User {
 }
 
 const Users = async () => {
+  // 1. Static Rendering 
   // Static rendering by fetch() default
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  // const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  
+  // 2. Disable cache
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    "cache": "no-store"
+  });
   const users: User[] = await res.json();
   return (
     <main>
       <h1>Users</h1>
-      {/* Static rendering => the ts won't change after built */}
+      {/* Dynamic rendering => ts updates every time it's refreshed */}
       <p>{new Date().toLocaleTimeString()}</p>
       <ul>{users.map(user => <li key={user.id}>{user.name}</li>)}</ul>
     </main>
   )
 }
 
-export default Users
+export default Users 

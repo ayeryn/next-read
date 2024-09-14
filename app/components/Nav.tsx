8609c685f78fe9f1ebb6dca9022f233ab7861d55
Eryn Li <erynli@utexas.edu>
2024-09-14 15:26:09 -0500

    update

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -4,7 +4,19 @@ import {useState, useEffect} from 'react';
 import {signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
+  // const isUserLoggedIn = false;
   const isUserLoggedIn = true;
+
+  const [providers, setProviders] = useState(null);
+  useEffect(() => {
+    const setAuthProviders = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    };
+  
+    setAuthProviders();
+  }, []);
+  
 
   return (
     <nav className="navbar w-full mb-16 pt-33">
@@ -13,27 +25,28 @@ const Nav = () => {
           Next Read
         </Link>
       </div>
-       <div className="flex-none">
-         <ul className="menu menu-horizontal px-1">
-           <li><Link href="/books">Books</Link></li>
-          </ul>
-        </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li><Link href="/books">Books</Link></li>
+        </ul>
+      </div>
       <div className="sm:flex hidden">
         {isUserLoggedIn ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/users">Users</Link>
-            <button className="btn btn-primary btn-sm" onClick={signOut}>
+            <button type="button" className="btn btn-primary btn-sm" onClick={signOut}>
                 Sign Out
               </button>
           </div>
         ) : (
+          
           <div className="flex gap-3 md:gap-5">
-              <button className="btn btn-primary btn-sm" onClick={()=>{}}>
-                Sign Up
-              </button>
-            </div>
+            <button type="button" className="btn btn-primary btn-sm" onClick={signIn}>
+              Sign In
+            </button>
+          </div>
         )}
-      </div>
+        </div>
     </nav>
   )
 }

9646b02857059fdd376928274ee914f4638d7f53
Eryn Li <erynli@utexas.edu>
2024-09-14 17:46:28 -0500

    connect to providers and DB

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Nav from "./components/Nav";
+import Provider from "./components/Provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,8 +28,10 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="lemonade">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {/* <Provider children={} session={}> */}
         <Nav />
         {children}
+        {/* </Provider> */}
       </body>
     </html>
   );

7da160672bfd17315f9ad3ce369bc669bd56e8ae
Eryn Li <erynli@utexas.edu>
2024-09-14 14:47:26 -0500

    add some hooks to Nav

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,6 +1,7 @@
 'use client';
-import React from 'react'
 import Link from "next/link";
+import {useState, useEffect} from 'react';
+import {signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
   return (

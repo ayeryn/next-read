b52631df1732fa80e16234dc4bddc4a4eb8a3563
Eryn Li <erynli@utexas.edu>
2024-09-14 10:13:11 -0500

    Adds interface User to extract response of the API request. closes #5

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const Users = () => {
+interface User {
+  id: number;
+  name: string;
+}
+
+const Users = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
   return (
-    <div>Users</div>
+    <main>
+      <h1>Users</h1>
+      <p>{users.map(users => users.name)}</p>
+    </main>
   )
 }
 

51e7884ef948ef3a0ec8f24ab8f1aac80806a8dc
Eryn Li <erynli@utexas.edu>
2024-09-14 10:29:52 -0500

    Add comments and notes on rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,11 +6,14 @@ interface User {
 }
 
 const Users = async () => {
+  // Static rendering by fetch() default
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const users: User[] = await res.json();
   return (
     <main>
       <h1>Users</h1>
+      {/* Static rendering => the ts won't change after built */}
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>{users.map(user => <li key={user.id}>{user.name}</li>)}</ul>
     </main>
   )

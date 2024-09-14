1d86d77c3dc3b4e499474529d7e07e5842bcb8dc
Eryn Li <erynli@utexas.edu>
2024-09-14 16:09:03 -0500

    Add conditional jsx for login/false

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,5 +1,6 @@
 'use client';
 import Link from "next/link";
+import Image from "next/image";
 import {useState, useEffect} from 'react';
 import {signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
@@ -7,7 +8,7 @@ const Nav = () => {
   // const isUserLoggedIn = false;
   const isUserLoggedIn = true;
 
-  const [providers, setProviders] = useState(null);
+  const [providers, setProviders] = useState<any>(null);
   useEffect(() => {
     const setAuthProviders = async () => {
       const response = await getProviders();
@@ -19,10 +20,16 @@ const Nav = () => {
   
 
   return (
-    <nav className="navbar w-full mb-16 pt-33">
+    <nav className="navbar bg-base-100 text-xl">
       <div className="flex-1">
         <Link href="/" className="flex gap-2 flex-center">
-          Next Read
+          <Image 
+                src="/assets/images/logo.svg"
+                alt="logo picture"
+                width={40}
+                height={40}
+                />
+          <p>Next Read</p>
         </Link>
       </div>
       <div className="flex-none">
@@ -30,25 +37,44 @@ const Nav = () => {
           <li><Link href="/books">Books</Link></li>
         </ul>
       </div>
+
+      {/* Mobile Navigation */}
       <div className="sm:flex hidden">
         {isUserLoggedIn ? (
           <div className="flex gap-3 md:gap-5">
-            <Link href="/users">Users</Link>
+            <Link href="/users">
+              Users
+            </Link>
+
             <button type="button" className="btn btn-primary btn-sm" onClick={signOut}>
                 Sign Out
               </button>
           </div>
         ) : (
-          
-          <div className="flex gap-3 md:gap-5">
-            <button type="button" className="btn btn-primary btn-sm" onClick={signIn}>
+          <>
+          {providers && 
+           Object.values(providers).map((provider: any) => (
+            <button 
+              type="button" 
+              key={provider.name} 
+              onClick={()=>signIn(provider.id)} 
+              className="btn btn-primary btn-sm">
               Sign In
             </button>
-          </div>
-        )}
+          ))}
+          </>
+          )}
         </div>
+
+        {/* Desktop Navigation */}
+        {/* <div className="sm:hidden flex relative">
+          {isUserLoggedIn ? (
+            <div className="flex">
+              </div>
+          ):()}
+        </div> */}
     </nav>
-  )
+  );
 }
 
 export default Nav

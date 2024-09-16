82eba3f539fc15d2c055f6f2d4916959f1be4ff0
Eryn Li <erynli@utexas.edu>
2024-09-16 09:46:17 -0500

    JK using one Nav component for the header. update fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,14 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import Nav from "./components/Nav";
 
 export const metadata: Metadata = {
   title: "Next Read",
   description: "Designed and Made by EL",
 };
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -26,9 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-theme="lemonade">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={inter.className}>
         <div className="flex h-full min-h-screen w-full flex-col justify-between">
-          <Header />
+          <Nav />
           <main className="mx-auto w-full max-w-3xl flex-auto px-4 py-4 sm:px-6 md:py-6">
             {children}
           </main>
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-import ProductCard from "./components/ProductCard/ProductCard";
+// import ProductCard from "./components/ProductCard/ProductCard";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-      <main className={styles.main}>
-        <h1 className="text-primary">What is your next read?</h1>
-        <p>Welcome to your personalized reading list!</p>
-        <ProductCard />
-      </main>
+    <main className={styles.main}>
+      <h1 className="text-primary text-4xl font-bold">
+        What is your next read?
+      </h1>
+      <p>Welcome to your personalized reading list!</p>
+      {/* <ProductCard /> */}
+    </main>
   );
 }

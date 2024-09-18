d690cb7a1d70b4922d335a31961f6694ab33ee09
Eryn Li <erynli@utexas.edu>
2024-09-17 19:46:07 -0500

    add Google provider and set auth signin page to custom page

--- auth.ts
+++ auth.ts
@@ -1,11 +1,13 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 // import connectToDb from "./lib/db";
 // import { User } from "./models/user";
 // import { compare } from "bcryptjs";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [Google],
+  pages: {
+    signIn: "/signin",
+  },
 });

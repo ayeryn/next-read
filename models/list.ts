fc8e2d5c0c7781b84efb8df7856e7543bbcd2661
Eryn Li <erynli@utexas.edu>
2024-09-26 17:02:16 -0500

    add timestamps to users and lists

--- models/list.ts
+++ models/list.ts
@@ -1,16 +1,21 @@
 import mongoose, { Schema } from "mongoose";
 
-const ListSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "List name is required!"],
-    // TODO: Find a way to make a listname unique to one user
+const listSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "List name is required!"],
+      // TODO: Find a way to make a listname unique to one user
+    },
+    description: { type: String },
+    creator: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-  description: { type: String },
-  creator: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-});
+  {
+    timestamps: { createdAt: "createdAt", updatedAt: "modifiedAt" }, // Enable timestamps
+  }
+);
 
-export const List = mongoose.models?.List || mongoose.model("List", ListSchema);
+export const List = mongoose.models?.List || mongoose.model("List", listSchema);

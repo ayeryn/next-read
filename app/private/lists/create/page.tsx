870b565059d605b2f192fe3a392395501ab52449
Eryn Li <erynli@utexas.edu>
2024-09-27 09:02:22 -0500

    No need to be async

--- app/private/lists/create/page.tsx
+++ app/private/lists/create/page.tsx
@@ -1,6 +1,6 @@
 import Form from "@/components/create-form";
 
-export default async function CreateList() {
+export default function CreateList() {
   return (
     <main>
       <div className="mt-10 max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white border border-[#121212]  dark:bg-black">

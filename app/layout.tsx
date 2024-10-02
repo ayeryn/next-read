934b3cb02fbfe9dd0e7af6c2b4982ad47601f79e
Eryn Li <erynli@utexas.edu>
2024-10-02 14:28:26 -0500

    remove max-w-3xl from main

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="flex h-full min-h-screen w-full flex-col justify-between">
           <Nav />
-          <main className="mx-auto w-full max-w-3xl flex-auto px-4 py-4 sm:px-6 md:py-6">
+          <main className="mx-auto w-full flex-auto px-4 py-4 sm:px-6 md:py-6">
             {children}
           </main>
         </div>

02717c12b00ce33074de501568f4e658ca4818c3
Eryn Li <erynli@utexas.edu>
2024-10-09 20:00:34 -0500

    update listSchema and create default list at user creation. closes @14

--- models/listBook.ts
+++ models/listBook.ts
@@ -1,15 +1,17 @@
 import mongoose, { Schema } from "mongoose";
 
-const bookListSchema = new mongoose.Schema(
-  {
-    googleId: { type: String, unique: true, required: true },
-    listId: {
-      type: Schema.Types.ObjectId,
-      ref: "List",
-    },
+const bookListSchema = new mongoose.Schema({
+  googleId: { type: String, unique: true, required: true },
+  listId: {
+    type: Schema.Types.ObjectId,
+    ref: "List",
   },
-  {}
-);
+  status: {
+    enum: ["read", "to-read", "reading"],
+    required: true,
+    default: "to-read",
+  },
+});
 
 export const BookList =
   mongoose.models?.BookList || mongoose.model("BookList", bookListSchema);
